0c8ed06d0516b16b061bf05729e3ca36f765eb39
dreambo <boudhaim@gmail.com>
2021-02-26 19:04:34 +0100

    add customer material

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -9,6 +9,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
 
 const matModules = [
   MatSliderModule,
@@ -20,7 +21,8 @@ const matModules = [
   MatTableModule,
   MatPaginatorModule,
   MatSortModule,
-  MatMenuModule
+  MatMenuModule,
+  MatInputModule
 ];
 
 @NgModule({
--- src/app/customer/material/customer-mat.component.ts
+++ src/app/customer/material/customer-mat.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTable } from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
+import { Customer } from '../../model/customer';
+import { CustomerService } from '../../service/customerservice';
+import { of } from 'rxjs';
 
 @Component({
-  selector: 'app-customer-mat',
   templateUrl: './customer-mat.component.html',
   styleUrls: ['./customer-mat.component.css']
 })
-export class CustomerMatComponent implements OnInit {
+export class CustomerMatComponent implements AfterViewInit, OnInit {
 
-  constructor() { }
+  @ViewChild(MatPaginator)
+  paginator?: MatPaginator;
 
-  ngOnInit(): void {
+  @ViewChild(MatSort)
+  sort?: MatSort;
+
+  @ViewChild(MatTable)
+  table?: MatTable<Customer>;
+
+  dataSource;
+  data: Customer[];
+  displayedColumns;
+  rowsPerPage = 8;
+
+  constructor(private customerService: CustomerService) {}
+
+  ngOnInit() {
+
+    this.customerService.getCustomersSmall1().subscribe(res => {
+      this.data = res.data;
+      console.log('this.data=', this.data);
+      //this.dataSource = new MatTableDataSource(this.data);
+      this.displayedColumns = res.data[0] && Object.keys(this.data[0]);
+    });
+
+    this.customerService.getCustomers1().subscribe(res => {
+      if (res && res.data) {
+        console.log('data:', res.data);
+        this.dataSource = new MatTableDataSource(res.data);
+        this.displayedColumns = res.data[0] && Object.keys(res.data[0]);
+      };
+    });
   }
 
+  ngAfterViewInit() {
+    this.dataSource && (this.dataSource.sort      = this.sort);
+    this.dataSource && (this.dataSource.paginator = this.paginator);
+    this.table      && (this.table.dataSource     = this.dataSource);
+  }
+
+  filter(key: string) {
+    console.log(key);
+    this.dataSource.filter = key && key.toLowerCase();
+  }
 }
